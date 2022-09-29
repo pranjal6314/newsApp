3299d8c0afdbc585802bf484773615f857deb02e
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-09-29 20:44:12 +0530

    adding Router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './component/Navbar';
 import News from './component/News';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
 export default class App extends Component {  
+
   render() {
     return (
-      <div>
-        <Navbar/>
-        <News pageSize={10} country="in" category='science'/>
-      </div>
+      <Router>
+        <div>
+          <Navbar/>
+          
+          <Routes>
+          <Route exact path="/" element={<News pageSize={10} country="in" category='general'/>} />
+          <Route exact path="/about" element={<News pageSize={10} country="in" category='about'/>} />
+          <Route exact path="/business" element={<News pageSize={10} country="in" category='business'/>} />
+          <Route exact path="/entertainment" element={<News pageSize={10} country="in" category='entertainment'/>} />
+          <Route exact path="/general" element={<News pageSize={10} country="in" category='general'/>} />
+          <Route exact path="/health" element={<News pageSize={10} country="in" category='health'/>} />
+          <Route exact path="/science" element={<News pageSize={10} country="in" category='science'/>} />
+          <Route exact path="/sports" element={<News pageSize={10} country="in" category='sports'/>} />
+          <Route exact path="/technology" element={<News pageSize={10} country="in" category='technology'/>} />
+        </Routes>
+        </div>
+      </Router>
     )
   }
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import {
+  Link
+}from "react-router-dom"
 export default class Navbar extends Component {
 //   static propTypes = {
 //     prop: PropTypes
@@ -11,25 +13,25 @@ export default class Navbar extends Component {
       <>
         <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">NewsApp</a>
+    <Link className="navbar-brand" to="/">NewsApp</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
-        <li className="nav-item"><a className="nav-link" href="/about">About</a>
+        <li className="nav-item"><Link className="nav-link" to="/about">About</Link>
         </li>
-        <li className="nav-item"><a className="nav-link" href="/">business</a>
+        <li className="nav-item"><Link className="nav-link" to="/business">business</Link>
         </li>
-        <li className="nav-item"><a className="nav-link" href="/about">entertainment</a></li>
-        <li className="nav-item"><a className="nav-link" href="/about">general</a></li>
-        <li className="nav-item"><a className="nav-link" href="/about">health</a></li>
-        <li className="nav-item"><a className="nav-link" href="/about">science</a></li>
-        <li className="nav-item"><a className="nav-link" href="/about">sports</a></li>
-        <li className="nav-item"><a className="nav-link" href="/about">technology</a></li>
+        <li className="nav-item"><Link className="nav-link" to="/entertainment">entertainment</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/general">general</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/health">health</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/science">science</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/sports">sports</Link></li>
+        <li className="nav-item"><Link className="nav-link" to="/technology">technology</Link></li>
        
         
       </ul>
